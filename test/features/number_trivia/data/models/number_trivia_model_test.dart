27a568ed054fc83c8db15302e340b020819304d2
Md. Belal Hossain <belal.cseai@gmail.com>
2023-07-31 17:16:45 +0600

    add models test

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_ca_tdd/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:flutter_ca_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   const tNumberTriviaModel = NumberTriviaModel(number: 1, text: "Test Text");
@@ -8,12 +12,50 @@ void main() {
   test(
     "should be a subclass of NumberTrivia entity",
     () async {
-      // arrange
-
-      // act
-
       // assert
       expect(tNumberTriviaModel, isA<NumberTrivia>());
     },
   );
+
+  group("fromJson", () {
+    test(
+      "should return a valid model when the JSON number is an integer",
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+
+    test("should return a valid model when the JSON number is an integer",
+        () async {
+      // arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('trivia_double.json'));
+      // act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      // assert
+      expect(result, tNumberTriviaModel);
+    });
+  });
+
+  group("toJson", () {
+    test(
+      "should return a JSON map containing the proper data",
+      () async {
+        // act
+        final result = tNumberTriviaModel.toJson();
+        // assert
+        final expectedMap = {
+          "text": "Test Text",
+          "number": 1,
+        };
+        expect(result, expectedMap);
+      },
+    );
+  });
 }
